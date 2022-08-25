00a7da5db9bea36efb43ea62f17c301400921c90
Neil Voss <vossman77@yahoo.com>
2022-08-25 14:29:24 -0500

    change sha256

--- Casks/cpdf.rb
+++ Casks/cpdf.rb
@@ -2,7 +2,7 @@ cask "cpdf" do
   arch arm: "ARM", intel: "Intel"
   version "2.5.1"
 
-  sha256 arm:   "582d8952b1c153e17094bba4abfee395c05a47782496ef01c4eaf711576c74bd",
+  sha256 arm:   "9522643499267a2e324dcbcc136f7ea31856ee82ac4fcc801e19d5d8ee3ac606"
          intel: "d82be8c8eff64b11a67879f63c7a5742ac34d035a71c5799c8714d9b9a96ba4d"
 
   url "https://github.com/coherentgraphics/cpdf-binaries/raw/v#{version}/OSX-#{arch}/cpdf" 
