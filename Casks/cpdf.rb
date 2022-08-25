ef01b5a05b9779563b2dce6f65c527c465f31126
Neil Voss <vossman77@yahoo.com>
2022-08-25 14:30:10 -0500

    forgot comma

--- Casks/cpdf.rb
+++ Casks/cpdf.rb
@@ -2,7 +2,7 @@ cask "cpdf" do
   arch arm: "ARM", intel: "Intel"
   version "2.5.1"
 
-  sha256 arm:   "9522643499267a2e324dcbcc136f7ea31856ee82ac4fcc801e19d5d8ee3ac606"
+  sha256 arm:   "9522643499267a2e324dcbcc136f7ea31856ee82ac4fcc801e19d5d8ee3ac606",
          intel: "d82be8c8eff64b11a67879f63c7a5742ac34d035a71c5799c8714d9b9a96ba4d"
 
   url "https://github.com/coherentgraphics/cpdf-binaries/raw/v#{version}/OSX-#{arch}/cpdf" 
