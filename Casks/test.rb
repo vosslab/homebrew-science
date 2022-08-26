7287a50ca19fd42a6f1a12b0f55ebfc50d7bc0f1
Neil Voss <vossman77@yahoo.com>
2022-08-26 16:26:04 -0500

    smallest cask that does nothing that I could make

---
+++ Casks/test.rb
@@ -0,0 +1,12 @@
+cask "test" do
+  version "1.0"
+
+  sha256 "78079b6b784adb34de52b3d75f421cd89a9964fd3faa13ec799b9b80ed32a5fc"
+  url "https://raw.githubusercontent.com/vosslab/homebrew-science/main/LICENSE"
+
+  name "Test"
+  desc "Test Cask"
+  homepage "https://www.example.com/"
+
+  stage_only true
+end
