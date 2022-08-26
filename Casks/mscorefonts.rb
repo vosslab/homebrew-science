e2bd36fd6cbd7bf70a00b7b4802537c1b89f8d47
Neil Voss <vossman77@yahoo.com>
2022-08-26 16:26:36 -0500

    use this to install all the Microsoft fonts

--- Casks/mscorefonts.rb
+++ Casks/mscorefonts.rb
@@ -1,20 +1,24 @@
-class MSCoreFonts < Formula
-  desc "Microsoft Core fonts for the Web"
-  homepage "https://sourceforge.net/projects/mscorefonts2/"
-  license "Microsoft font license"
+cask "mscorefonts" do
   version '2.0'
 
-  depends_on "font-andale-mono"
-  depends_on "font-arial"
-  depends_on "font-arial-black"
-  depends_on "font-baskervville"
-  depends_on "font-comic-sans-ms"
-  depends_on "font-courier-new"
-  depends_on "font-georgia"
-  depends_on "font-impact"  
-  depends_on "font-times-new-roman"
-  depends_on "font-trebuchet-ms"  
-  depends_on "font-verdana"
+  sha256 "78079b6b784adb34de52b3d75f421cd89a9964fd3faa13ec799b9b80ed32a5fc"
+  url "https://raw.githubusercontent.com/vosslab/homebrew-science/main/LICENSE"
+
+  desc "Microsoft Core fonts for the Web"
+  homepage "https://sourceforge.net/projects/mscorefonts2/"
+
+  depends_on formula: "unar"
+  depends_on cask: "font-andale-mono"
+  depends_on cask: "font-arial"
+  depends_on cask: "font-arial-black"
+  depends_on cask: "font-baskervville"
+  depends_on cask: "font-comic-sans-ms"
+  depends_on cask: "font-courier-new"
+  depends_on cask: "font-georgia"
+  depends_on cask: "font-impact"  
+  depends_on cask: "font-times-new-roman"
+  depends_on cask: "font-trebuchet-ms"  
+  depends_on cask: "font-verdana"
 
   stage_only true
 
