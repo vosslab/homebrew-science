f9bb5d61e7ed78f6142eb1a58ba1c7b9025c19b1
Neil Voss <vossman77@yahoo.com>
2022-08-29 20:57:52 -0500

    macos 12.5.1 rejects cpdf v2.5.1, so downgrade to v2.4

--- Casks/cpdf.rb
+++ Casks/cpdf.rb
@@ -1,9 +1,11 @@
 cask "cpdf" do
   arch arm: "ARM", intel: "Intel"
-  version "2.5.1"
+  version "2.4"
 
-  sha256 arm:   "9522643499267a2e324dcbcc136f7ea31856ee82ac4fcc801e19d5d8ee3ac606",
-         intel: "d82be8c8eff64b11a67879f63c7a5742ac34d035a71c5799c8714d9b9a96ba4d"
+  #e.g. https://github.com/coherentgraphics/cpdf-binaries/raw/v2.4/OSX-ARM/cpdf
+
+  sha256 arm:   "e01dd64a9f73fc038eeb689d05ce67e5e786ebc0660f3e310b45bdc136d499aa",
+         intel: "8ddd3aac989cca5bdf9734dea233ba026a029d82b0e4fa745c7115de3519ffd8"
 
   url "https://github.com/coherentgraphics/cpdf-binaries/raw/v#{version}/OSX-#{arch}/cpdf" 
 
