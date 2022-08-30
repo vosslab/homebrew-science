e4905685d9d25ae26b83bddd37dd6dc3f4b965b8
Neil Voss <vossman77@yahoo.com>
2022-08-29 21:01:35 -0500

    whitespace

--- Casks/cpdf.rb
+++ Casks/cpdf.rb
@@ -7,7 +7,7 @@ cask "cpdf" do
   sha256 arm:   "e01dd64a9f73fc038eeb689d05ce67e5e786ebc0660f3e310b45bdc136d499aa",
          intel: "8ddd3aac989cca5bdf9734dea233ba026a029d82b0e4fa745c7115de3519ffd8"
 
-  url "https://github.com/coherentgraphics/cpdf-binaries/raw/v#{version}/OSX-#{arch}/cpdf" 
+  url "https://github.com/coherentgraphics/cpdf-binaries/raw/v#{version}/OSX-#{arch}/cpdf"
 
   desc "Coherent PDF Command-line Tools to modify PDF files"
   homepage "https://www.coherentpdf.com/"
