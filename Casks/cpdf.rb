f58c93efc6d2204a5f8ecc6e19ddf2dd295863b3
Neil Voss <vossman77@yahoo.com>
2023-02-06 13:17:29 -0600

    switch to master

--- Casks/cpdf.rb
+++ Casks/cpdf.rb
@@ -1,13 +1,14 @@
 cask "cpdf" do
   arch arm: "ARM", intel: "Intel"
-  version "2.5.1"
+  version "master"
 
   #e.g. https://github.com/coherentgraphics/cpdf-binaries/raw/v2.4/OSX-ARM/cpdf
+  #e.g. https://github.com/coherentgraphics/cpdf-binaries/raw/master/OSX-ARM/cpdf
 
-  sha256 arm:   "9522643499267a2e324dcbcc136f7ea31856ee82ac4fcc801e19d5d8ee3ac606",
-         intel: "d82be8c8eff64b11a67879f63c7a5742ac34d035a71c5799c8714d9b9a96ba4d"
+  sha256 arm:   "6e510d68329cc26e9f890e1d2f4d9559f92e9f6c255dff37b0ef57e71b47bb19",
+         intel: "2a574f11fe30bea34d321a1430c3e61fe087975f67f2a35c50c9e7e8e29aca40"
 
-  url "https://github.com/coherentgraphics/cpdf-binaries/raw/v#{version}/OSX-#{arch}/cpdf"
+  url "https://github.com/coherentgraphics/cpdf-binaries/raw/#{version}/OSX-#{arch}/cpdf"
 
   desc "Coherent PDF Command-line Tools to modify PDF files"
   homepage "https://www.coherentpdf.com/"
